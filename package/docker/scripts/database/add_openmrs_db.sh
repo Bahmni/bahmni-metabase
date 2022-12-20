56124f8a2cc12cecc3a588488450c9d0dacf2e0e
MeSusmeeta <susmeeta.jagtap@thoughtworks.com>
2022-12-20 18:25:59 +0530

    Refactor. code for connecting to multiple database

--- package/docker/scripts/database/add_openmrs_db.sh
+++ package/docker/scripts/database/add_openmrs_db.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-echo "Adding OPENMRS Database to Metabase"
+echo "Adding Databases to Metabase"
 
-# echo ${CONNECT_TO_DB}
-# echo "********1"
-# echo "${CONNECT_TO_DB}"
-# echo "********2"
-# echo $CONNECT_TO_DB
-# echo "********3"
-# echo "$CONNECT_TO_DB"
-# echo "********4"
-
-CONNECT_TO_DB=${CONNECT_TO_DB}
-
-echo "connect to DB : ${CONNECT_TO_DB}"
+DB_CONNECTIONS=$DB_CONNECTIONS
+if !(jq -e . >/dev/null 2>&1 <<< $DB_CONNECTIONS); then
+    echo "Database connections cannot be provided for invalid json value. DB_CONNECTIONS: $DB_CONNECTIONS"
+    exit 1
+fi
 
 add_DB_to_metabase(){
     database_response=$(curl -s -w "%{http_code}" -X POST \
@@ -21,42 +14,40 @@ add_DB_to_metabase(){
     -H "X-Metabase-Session: ${MB_TOKEN}" \
     http://${MB_HOST}:${MB_PORT}/api/database \
     -d '{
-        "engine": "'$5}'",
-        "name": "'$1'",
+        "engine": "'$1'",
+        "name": "'$2'",
         "details": {
-            "host": "'$1'",
-            "db": "'$2'",
-            "user": "'$3'",
-            "password": "'$4'"
+            "host": "'$2'",
+            "db": "'$3'",
+            "user": "'$4'",
+            "password": "'$5'"
         }
     }')
     STATUS=${database_response: -3}
-    echo $STATUS
-
+${DB_NAME}+”_ID” = (jq database_response)
     if [ $STATUS == 200 ]
     then
         echo "$2 Database added to Metabase" 
     else
-        echo "ERROR OCCOURED WHILE CONNECT DB : $2" 
+        echo "error occured wile connecting DB : $2" 
     fi
 }
 
-length=$(jq '. | length' <<< "${value}" )
+echo "Database connections : $DB_CONNECTIONS"
+
+length=$(jq '. | length' <<< ${DB_CONNECTIONS} )
 
 for ((i=0; i < $length; i++))
 do
-    DB_DATA=$(jq ".[$i]" <<< "${value}")
+    DB_DATA=$(jq ".[$i]" <<< ${DB_CONNECTIONS})
+
     DB_HOST_NAME=$(echo "${DB_DATA}" | jq '.DB_HOST' | tr -d '"') 
     DB_NAME=$(echo "${DB_DATA}" | jq '.DB_NAME' | tr -d '"') 
     DB_USERNAME=$(echo "${DB_DATA}" | jq '.DB_USERNAME' | tr -d '"') 
     DB_PASSWORD=$(echo "${DB_DATA}" | jq '.DB_PASSWORD' | tr -d '"') 
     DB_TYPE=$(echo "${DB_DATA}" | jq '.DB_TYPE' | tr -d '"') 
-    # echo $DB_HOST_NAME
-    # echo $DB_NAME
-    # echo $DB_USERNAME
-    # echo $DB_PASSWORD
-    # echo $DB_TYPE
     echo "connecting DB : $DB_DATA"
-    add_DB_to_metabase $DB_HOST_NAME $DB_NAME $DB_USERNAME $DB_PASSWORD $DB_TYPE
+
+    add_DB_to_metabase  $DB_TYPE $DB_HOST_NAME $DB_NAME $DB_USERNAME $DB_PASSWORD
 done
 
--- package/docker/scripts/metabase/metabase_startup.sh
+++ package/docker/scripts/metabase/metabase_startup.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 echo "Starting and intializing the Metabase."
 
-echo ${CONNECT_TO_DB}
-echo "********1"
-echo "${CONNECT_TO_DB}"
-echo "********2"
-echo $CONNECT_TO_DB
-echo "********3"
-echo "$CONNECT_TO_DB"
-echo "********4"
-
 /app/run_metabase.sh & /app/scripts/metabase/metabase_init.sh
 
 wait ${!}
